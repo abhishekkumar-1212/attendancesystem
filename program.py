49de60ec89d62088442d2246692676d05ccc57fe
abhishekkumar-1212 <129652028+abhishekkumar-1212@users.noreply.github.com>
2024-05-01 18:41:22 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,11 @@
+# import cv2
+
+# video_cap = cv2.VideoCapture(0)
+# while True:
+#     ret, video_data = video_cap.read()
+#     cv2.imshow("video_live", video_data)
+#     if cv2.waitKey(10) == ord("e"):  # Press 'e' to exit
+#         break
+
+# video_cap.release()
+# cv2.destroyAllWindows()  # Close all windows when done
